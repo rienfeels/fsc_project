f0a2d99ce948961ef0bdf365d1cdb153cec35d53
RyanFields <rienfeels14@gmail.com>
2024-04-09 00:01:11 -0400

    help me

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -202,11 +202,9 @@ SIMPLE_JWT = {
     'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
 }
 
-CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOW_ALL_ORIGINS = True
 
-CORS_ALLOWED_ORIGINS = [
-    "https://66147e2885a4c00008b96562--fscapp.netlify.app",
-]
+
 
 CORS_ALLOW_CREDENTIALS = True
 
